e058c918984f6881b479864dc8444476fab7a00d
Brendan Coles <bcoles@gmail.com>
2015-11-21 17:18:13 +0000

    Add test constants

---
+++ test/common/constants.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+#
+# Copyright (c) 2015 Brendan Coles <bcoles@gmail.com>
+# SSRF Proxy - https://github.com/bcoles/ssrf_proxy
+# See the file 'LICENSE' for copying permission
+#
+
+SSRF_DEFAULT_OPTS = {
+  'proxy'          => '',
+  'method'         => 'GET',
+  'post_data'      => '',
+  'rules'          => '',
+  'ip_encoding'    => '',
+  'match'          => "\\A(.+)\\z",
+  'strip'          => '',
+  'guess_mime'     => false,
+  'guess_status'   => false,
+  'forward_cookies'=> false,
+  'body_to_uri'    => false,
+  'auth_to_uri'    => false,
+  'cookies_to_uri' => false,
+  'cookie'         => '',
+  'timeout'        => 10,
+  'user_agent'     => 'Mozilla/5.0',
+  'insecure'       => false
+}
+
+SERVER_DEFAULT_OPTS = {
+ 'interface' => '127.0.0.1',
+ 'port' => 8081
+}
+
