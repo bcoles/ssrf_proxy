4e683fe03a121027408f09adfa6f21d4d3a1941f
Brendan Coles <bcoles@gmail.com>
2017-12-21 10:56:53 +0000

    Allow detect_headers to detect multiple sets of headers

--- lib/ssrf_proxy/http.rb
+++ lib/ssrf_proxy/http.rb
@@ -768,29 +768,36 @@ module SSRFProxy
       # detect headers in response body
       if @detect_headers
         headers = ''
-        if result['body'] =~ %r{\AHTTP/(1\.\d) (\d+) (.*?)\r?\n(.*?)\r?\n\r?\n}m
-          logger.info('Parsing HTTP response headers in response body.')
-          version = $1
-          code = $2
-          message = $3
-          $4.split(/\r?\n/).each do |line|
-            if line =~ /^[A-Za-z0-9\-_]+: /
-              headers << "#{line}\n"
+        head = result['body'][0..8192] # use first 8192 byes
+        detected_headers = head.scan(%r{HTTP/(1\.\d) (\d+) (.*?)\r?\n(.*?)\r?\n\r?\n}m)
+
+        if detected_headers.empty?
+          logger.info('Found no HTTP response headers in response body.')
+        else
+          # HTTP redirects may contain more than one set of HTTP response headers
+          # Use the last
+          logger.info("Found #{detected_headers.count} sets of HTTP response headers in reponse. Using last.")
+          version = detected_headers.last[0]
+          code = detected_headers.last[1]
+          message = detected_headers.last[2]
+          detected_headers.last[3].split(/\r?\n/).each do |line|
+            if line =~ /^[A-Za-z0-9\-_\.]+: /
+              k = line.split(': ').first
+              v = line.split(': ')[1..-1]
+              headers << "#{k}: #{v}\n"
             else
               logger.warn('Could not use response headers in response body : Headers are malformed.')
               headers = ''
               break
             end
           end
-        else
-          logger.info('Found no HTTP response headers in response body.')
         end
         unless headers.eql?('')
           result['http_version'] = version
           result['code'] = code.to_i
           result['message'] = message
           result['headers'] = headers
-          result['body'] = result['body'].split(/\r?\n\r?\n/)[1..-1].flatten.join("\n\n")
+          result['body'] = result['body'].split(/\r?\n\r?\n/)[detected_headers.count..-1].flatten.join("\n\n")
         end
       end
 
